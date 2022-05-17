670aadefe4d43e3e8feff694c214a3c4105dfaea
Abdias Chafang <chafang.abdias@liss-sarl.com>
2022-05-17 14:32:44 +0200

    print a store data to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { changeUserName, initAction } from 'src/state/01-actions';
 
 @Component({
@@ -10,6 +11,8 @@ import { changeUserName, initAction } from 'src/state/01-actions';
 
 export class AppComponent implements OnInit {
   title='starter-ngrx';
+  test = 55.22
+  public user: Observable<any>={} as Observable<any>;
 
   constructor(private store: Store) {
 
@@ -17,6 +20,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(initAction());
+    this.user = this.store.select((state: any) => state.root.user);
   }
 
   public changeUserName=(): void => {
