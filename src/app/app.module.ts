7fa929faa3a6ca805251772ebe89a3a953bdb079
lineDjuissi <linedjuissi@yahoo.fr>
2022-08-14 09:29:13 +0200

    ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { metaReducers, rootReducer } from 'src/app/state/00-reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { metaReducers, rootReducer } from 'src/app/state/00-reducer';
       [ROOT_FEATURE_KEY]: rootReducer
     }, {
       metaReducers: metaReducers
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
