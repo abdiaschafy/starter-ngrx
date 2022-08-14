bd91c9fea1ac03b7a2505048e2c1a4cc69d435f4
lineDjuissi <linedjuissi@yahoo.fr>
2022-08-14 10:47:26 +0200

    create multiple action

--- src/app/state/01-actions.ts
+++ src/app/state/01-actions.ts
@@ -1,6 +1,12 @@
+import { User } from './../models/user';
 import { createAction, props } from "@ngrx/store";
+import { HttpErrorResponse } from "@angular/common/http";
 
 export const initAction=createAction('INIT_APP');
 
 export const changeUserName=createAction('[ROOT] CHANGE_USERNAME', props<{username: string}>());
-export const changeIsAdmin=createAction('[ROOT] CHANGE_IS_ADMIN', props<{isAdmin: boolean}>());
+export const changeIsAdmin=createAction('[ROOT] CHANGE_IS_ADMIN', props<{ isAdmin: boolean }>());
+
+export const loadUsers=createAction('[USERS_API] LOAD_USERS');
+export const loadUsersSuccess=createAction('[USERS_API] LOAD_USERS_SUCCESS', props<{ users: User[] }>());
+export const loadUsersError=createAction('[USERS_API] LOAD_USERS_ERROR', props<{ error: HttpErrorResponse | Error | string }>());
